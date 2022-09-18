2c397c78fcbc9addc4b521b852146cc96784af57
MyNameJeremy <original.jeremy.grotjahn@gmail.com>
2022-09-18 22:01:24 +0200

    added text field for font name to import

--- src/extension.ts
+++ src/extension.ts
@@ -24,15 +24,18 @@ export function activate(context: vscode.ExtensionContext) {
 
 
 	let insertCommand = vscode.commands.registerCommand('googlefontimporter.testInsert', () => {
-		const editor = vscode.window.activeTextEditor;
-		if (editor) {
-			editor.edit(editBuilder => {
-				editBuilder.insert(editor.selection.active, "@import url('https://fonts.googleapis.com/css2?family=Roboto');");
-			});
-		}
+		vscode.window.showInputBox().then((fontName) => {
+			const editor = vscode.window.activeTextEditor;
+			if (editor) {
+				editor.edit(editBuilder => {
+					editBuilder.insert(editor.selection.active, "@import url('https://fonts.googleapis.com/css2?family=" + fontName + "');");
+				});
+			}
+		});
 	});
 
 	context.subscriptions.push(insertCommand);
+
 }
 
 // this method is called when your extension is deactivated
