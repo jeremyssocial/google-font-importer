74c3c1513954c1d90db70e89371626caf095d9f3
MyNameJeremy <original.jeremy.grotjahn@gmail.com>
2022-09-19 23:07:20 +0200

    changed import link to old version (not css2)

--- src/extension.ts
+++ src/extension.ts
@@ -1,11 +1,46 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+import { url } from 'inspector';
 import { text } from 'stream/consumers';
 import * as vscode from 'vscode';
+// eslint-disable-next-line @typescript-eslint/naming-convention
+let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	const http = new XMLHttpRequest();
+
+	function checkUrl(url: string) {
+		http.open('HEAD', url, false);
+		http.send();
+		if (http.status === 400) {
+			return false;
+		}
+		return true;
+	}
+
+	function addWeights(fontName: string) {
+		let useWeights = [];
+		let url = "https://fonts.googleapis.com/css?family=" + fontName?.replace(" ", "+");
+		for (let weight = 100; weight <= 900; weight += 100) {
+			if (checkUrl(url + ":" + weight)) {
+				useWeights.push(weight);
+			}
+			if (checkUrl(url + ":" + weight + "i")) {
+				useWeights.push(weight + "i");
+			}
+		}
+		if (useWeights.length !== 0) {
+			url = url + ":";
+			useWeights.forEach(weight => {
+				url = url + weight + ",";
+			});
+			url = url.slice(0, -1);
+		}
+		return url;
+	}
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -27,9 +62,23 @@ export function activate(context: vscode.ExtensionContext) {
 		vscode.window.showInputBox().then((fontName) => {
 			const editor = vscode.window.activeTextEditor;
 			if (editor) {
-				editor.edit(editBuilder => {
-					editBuilder.insert(editor.selection.active, "@import url('https://fonts.googleapis.com/css2?family=" + fontName + "');");
-				});
+				if (fontName) {
+
+					let importUrl = "https://fonts.googleapis.com/css?family=" + fontName.replace(" ", "+");
+					if (checkUrl(importUrl)) {
+						let url = addWeights(fontName);
+						editor.edit(editBuilder => {
+							editBuilder.insert(editor.selection.active, "@import url('" + url + "');");
+						});
+					}
+					else {
+						vscode.window.showInformationMessage('The requested font families "' + fontName + '" are not available on Google Fonts.');
+					}
+				}
+				else {
+					vscode.window.showInformationMessage('Font name can\'t be empty');
+				}
+
 			}
 		});
 	});
