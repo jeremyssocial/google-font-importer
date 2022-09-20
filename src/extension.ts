ca63c3153ea0c684a9f533f96cb06cf6955f72d4
MyNameJeremy <original.jeremy.grotjahn@gmail.com>
2022-09-20 20:54:54 +0200

    removed some tutorial code

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,9 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import { url } from 'inspector';
 import { text } from 'stream/consumers';
 import * as vscode from 'vscode';
 // eslint-disable-next-line @typescript-eslint/naming-convention
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
 	const http = new XMLHttpRequest();
@@ -42,22 +38,6 @@ export function activate(context: vscode.ExtensionContext) {
 		return url;
 	}
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "googlefontimporter" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('googlefontimporter.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from googleFontImporter!');
-	});
-
-	context.subscriptions.push(disposable);
-
-
 	let insertCommand = vscode.commands.registerCommand('googlefontimporter.importFont', () => {
 		vscode.window.showInputBox().then((fontName) => {
 			const editor = vscode.window.activeTextEditor;
@@ -87,5 +67,5 @@ export function activate(context: vscode.ExtensionContext) {
 
 }
 
-// this method is called when your extension is deactivated
+
 export function deactivate() { }
